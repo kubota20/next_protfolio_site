1bd2a3b3404a2c34b9a06ef2454da7c6a504e517
kubota20 <doc0000doc77@gmail.com>
2024-06-12 17:07:49 +0900

    use client用のLayout作成

---
+++ src/app/admin/(dashboard)/(routes)/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  return <>{children}</>;
+}
