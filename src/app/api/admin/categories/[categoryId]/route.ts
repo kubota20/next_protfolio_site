8a50208cf3300bcb508bff77f7d517a57e4b9bee
kubota20 <doc0000doc77@gmail.com>
2024-06-12 16:41:33 +0900

    category用 API Handlers作成

---
+++ src/app/api/admin/categories/[categoryId]/route.ts
@@ -0,0 +1,87 @@
+import { prismadb } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+// 取得
+export async function GET(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    // categoryIdに問題がある場合
+    if (!params.categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    // findUnique 取得 一意の識別子またはIDを指定する必要がある
+    const category = await prismadb.category.findUnique({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    // id 問題がある場合
+    if (!category) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    return NextResponse.json({ category }, { status: 201 });
+  } catch (error) {
+    console.log("[CATEGORY_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+// 変更
+export async function PATCH(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    const { name } = await req.json();
+
+    // categoryIdに問題がある場合
+    if (!params.categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    // update 更新 条件に一致するレコードを更新,一意の識別子またはIDを指定する必要がある
+    const category = await prismadb.category.update({
+      where: {
+        id: params.categoryId,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json({ category }, { status: 201 });
+  } catch (error) {
+    console.log("[CATEGORY_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+// 消す
+export async function DELETE(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    // categoryIdに問題がある場合
+    if (!params.categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    // deleteMany 複数件削除 条件に一致する全てのレコードを削除する
+    const category = await prismadb.category.deleteMany({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    return NextResponse.json({ category }, { status: 201 });
+  } catch (error) {
+    console.log("[CATEGORY_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ src/app/api/admin/categories/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+
+import { prismadb } from "@/lib/prisma";
+
+// 渡す
+export async function POST(req: Request) {
+  try {
+    const { name } = await req.json();
+
+    if (!name) {
+      return new NextResponse("name is required", { status: 400 });
+    }
+
+    // create 作成
+    const category = await prismadb.category.create({
+      data: {
+        name: name,
+      },
+    });
+
+    return NextResponse.json({ category }, { status: 201 });
+  } catch (error) {
+    console.log("[CATEGORY_POST]", error);
+
+    return new NextResponse("Interal error", { status: 500 });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    //　findMany 複数件取得
+    const categories = await prismadb.category.findMany({});
+
+    return NextResponse.json({ categories }, { status: 200 });
+  } catch (error) {
+    console.log("[CATEGORIED_POST]", error);
+
+    return new NextResponse("Interal error", { status: 500 });
+  }
+}
