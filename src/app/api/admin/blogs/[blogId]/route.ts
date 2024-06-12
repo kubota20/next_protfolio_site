908473664967654db8d63958055eec24bb3e0982
kubota20 <doc0000doc77@gmail.com>
2024-06-12 16:41:14 +0900

    blog用 API Handlers作成

---
+++ src/app/api/admin/blogs/[blogId]/route.ts
@@ -0,0 +1,111 @@
+import { NextResponse } from "next/server";
+import { prismadb } from "@/lib/prisma";
+
+// 取得
+export async function GET(
+  req: Request,
+  { params }: { params: { blogId: string } }
+) {
+  try {
+    // blogIdに問題がある場合
+    if (!params.blogId) {
+      return new NextResponse("blog id is required", { status: 400 });
+    }
+    // findUnique 取得 一意の識別子またはIDを指定する必要がある
+    const blog = await prismadb.blog.findUnique({
+      where: {
+        id: params.blogId,
+      },
+      include: {
+        // category取り出し
+        category: true,
+      },
+    });
+
+    return NextResponse.json({ blog }, { status: 201 });
+  } catch (error) {
+    console.log("[BLOG_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+// 変更
+export async function PATCH(
+  req: Request,
+  { params }: { params: { blogId: string } }
+) {
+  try {
+    const { title, discription, link, categoryId, imageUrl, release } =
+      await req.json();
+
+    // blogIdに問題がある場合
+    if (!params.blogId) {
+      return new NextResponse("blog id is required", { status: 400 });
+    }
+
+    if (!title) {
+      return new NextResponse("title is required", { status: 400 });
+    }
+
+    if (!discription) {
+      return new NextResponse("discription is required", { status: 400 });
+    }
+
+    if (!link) {
+      return new NextResponse("link is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("imageUrl is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("categoryId is required", { status: 400 });
+    }
+
+    const blog = await prismadb.blog.update({
+      where: {
+        id: params.blogId,
+      },
+      data: {
+        title,
+        discription,
+        link,
+        imageUrl,
+        categoryId,
+        release,
+      },
+    });
+
+    return NextResponse.json({ blog }, { status: 201 });
+  } catch (error) {
+    console.log("[BLOG_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+// 削除
+export async function DELETE(
+  req: Request,
+  { params }: { params: { blogId: string } }
+) {
+  try {
+    // blogIdに問題がある場合
+    if (!params.blogId) {
+      return new NextResponse("blog id is required", { status: 400 });
+    }
+
+    // deleteMany 複数件削除 条件に一致する全てのレコードを削除する
+    const blog = await prismadb.blog.deleteMany({
+      where: {
+        // idだけ消せば全部消える
+        id: params.blogId,
+      },
+    });
+
+    return NextResponse.json({ blog }, { status: 201 });
+  } catch (error) {
+    console.log("[BLOG_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ src/app/api/admin/blogs/route.ts
@@ -0,0 +1,81 @@
+import { NextResponse } from "next/server";
+
+import { prismadb } from "@/lib/prisma";
+
+// 渡す
+export async function POST(req: Request) {
+  try {
+    const { title, discription, link, categoryId, imageUrl, release } =
+      await req.json();
+
+    if (!title) {
+      return new NextResponse("title is required", { status: 400 });
+    }
+
+    if (!discription) {
+      return new NextResponse("discription is required", { status: 400 });
+    }
+
+    if (!link) {
+      return new NextResponse("link is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("imageUrl is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("categoryId is required", { status: 400 });
+    }
+
+    // create 作成
+    const blog = await prismadb.blog.create({
+      data: {
+        title,
+        discription,
+        link,
+        categoryId,
+        imageUrl,
+        release,
+      },
+    });
+
+    return NextResponse.json({ blog }, { status: 201 });
+  } catch (error) {
+    console.log("[BLOG_POST]", error);
+
+    return new NextResponse("Interal error", { status: 500 });
+  }
+}
+
+// ゲット
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    // http://localhost:3000/about/categories/[categoryId] の[categoryId]を貰います
+    const categoryId = searchParams.get("categoryId") || undefined;
+
+    //　findMany 複数件取得
+    const blogs = await prismadb.blog.findMany({
+      where: {
+        categoryId,
+        release: true,
+      },
+      include: {
+        // category を取り出す
+        category: true,
+      },
+      orderBy: {
+        // 順番
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json({ blogs }, { status: 201 });
+  } catch (error) {
+    console.log("[BLOGS_POST]", error);
+
+    return new NextResponse("Interal error", { status: 500 });
+  }
+}
