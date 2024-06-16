59bd4458c43734bd894430b50a0d46bee673468b
kubota20 <doc0000doc77@gmail.com>
2024-06-16 12:48:48 +0900

    コピー用

--- src/app/admin/(dashboard)/(routes)/blogs/components/columns.tsx
+++ src/app/admin/(dashboard)/(routes)/blogs/components/columns.tsx
@@ -4,6 +4,23 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { ColumnDef } from "@tanstack/react-table";
 import { CellAction } from "./call-action";
 
+import { SquareChevronDown, SquareChevronUp, Copy } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+
+const onClickCopy = (text: string) => {
+  navigator.clipboard.writeText(text);
+  toast.success("コピーしました");
+};
+
 export type BlogColumnProps = {
   id: string;
   title: string;
@@ -50,16 +67,59 @@ export const columns: ColumnDef<BlogColumnProps>[] = [
     accessorKey: "discription",
     enableHiding: false,
     header: "説明",
+    cell: ({ row }) => (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center justify-between">
+            <div className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[100px]">
+              {row.getValue("discription")}
+            </div>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <SquareChevronDown className="h-4 w-4" />
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="center" className="w-[300px]">
+          <div className="flex items-center justify-between">
+            <DropdownMenuLabel>{row.getValue("title")}</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <SquareChevronUp className="h-4 w-4" />
+            </DropdownMenuItem>
+          </div>
+          <div className="px-2">{row.getValue("discription")}</div>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    ),
   },
   {
     accessorKey: "link",
     header: "リンク",
-    cell: ({ row }) => <div>{row.getValue("link")}</div>,
+    cell: ({ row }) => (
+      <div className="flex items-center justify-between">
+        <div className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[100px]">
+          {row.getValue("link")}
+        </div>
+        <Copy
+          className="h-4 w-4"
+          onClick={() => onClickCopy(row.getValue("link"))}
+        />
+      </div>
+    ),
   },
   {
     accessorKey: "imageUrl",
     header: "画像URL",
-    cell: ({ row }) => <div>{row.getValue("imageUrl")}</div>,
+    cell: ({ row }) => (
+      <div className="flex items-center justify-between">
+        <div className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[100px]">
+          {row.getValue("imageUrl")}
+        </div>
+        <Copy
+          className="h-4 w-4"
+          onClick={() => onClickCopy(row.getValue("imageUrl"))}
+        />
+      </div>
+    ),
   },
   {
     accessorKey: "category",
