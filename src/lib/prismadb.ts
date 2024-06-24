7c12c7afff502566ea214ebb98cd97160d23acd7
kubota20 <doc0000doc77@gmail.com>
2024-06-24 16:06:58 +0900

    修正

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,32 +1,9 @@
-// 開発環境に不要なPrismaClientのインスタンスが追加されないようにする
-
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-let prismadb: PrismaClient | undefined;
+const prismadb = globalForPrisma.prisma || new PrismaClient();
 
-// 開発環境と本番環境を区別するためのフラグ
-const isDev = process.env.NODE_ENV === "development";
-
-// PrismaClientのインスタンスを取得する関数
-export function getPrismaClient(): PrismaClient {
-  if (!prismadb) {
-    prismadb = new PrismaClient();
-  }
-  return prismadb;
-}
-
-// 開発環境の場合のみPrismaClientをグローバルに設定
-if (isDev) {
-  globalThis.prisma = getPrismaClient();
-}
-
-// PrismaClientのクローズ処理を追加する
-process.on("beforeExit", () => {
-  prismadb?.$disconnect();
-});
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismadb;
 
 export default prismadb;
