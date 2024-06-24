70befe1feae6dc878fc5f904cfa9e6a6b434f918
kubota20 <doc0000doc77@gmail.com>
2024-06-24 12:32:22 +0900

    fetch移動

--- src/actions/get-blogs.tsx
+++ src/actions/get-blogs.tsx
@@ -3,12 +3,11 @@ import { Blog } from "@/types/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/api/admin/blogs`;
 
 export const getBlogs = async (): Promise<Blog[]> => {
+  const res = await fetch(URL, {
+    // SSR
+    cache: "no-store",
+  });
   try {
-    const res = await fetch(URL, {
-      // SSR
-      cache: "no-store",
-    });
-
     if (!res.ok) {
       throw new Error(`Fetch failed with status ${res.status}`);
     }
